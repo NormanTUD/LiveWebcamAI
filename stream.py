333eebc2e2d38b49816598adb52d5a3945cc66f3
n <norman.koch@tu-dresden.de>
2025-07-26 20:51:28 +0200

    removed stream

--- stream.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import requests
-
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    _, img = cv2.imencode('.jpg', frame)
-    requests.post("http://localhost:8000/upload", files={"file": ("frame.jpg", img.tobytes(), "image/jpeg")})
